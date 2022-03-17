28ec64e92d695a956445e32301d2a11299380121
weraleto <52561921+weraleto@users.noreply.github.com>
2022-03-17 16:47:50 +0300

    add translated cards animation [v1]

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -29,6 +29,30 @@ function startAnimFrameAnimation(rect) {
   window.requestAnimationFrame(step);
 }
 
+function translatedCardAnimationIn(event) {
+  let card = event.target
+  let cardFront = card.querySelector('.translated-card__item--front')
+  let cardOverlay = card.querySelector('.translated-card__item--overlay')
+  cardFront.classList.remove('animation-fade-in')
+  cardFront.classList.add('animation-fade-out')
+  setTimeout(function(){
+    cardOverlay.classList.remove('animation-fade-out')
+    cardOverlay.classList.add('animation-fade-in')
+  }, 500)
+}
+function translatedCardAnimationOut(event) {
+  let card = event.target
+  let cardFront = card.querySelector('.translated-card__item--front')
+  let cardOverlay = card.querySelector('.translated-card__item--overlay')
+  cardOverlay.classList.remove('animation-fade-in')
+  cardOverlay.classList.add('animation-fade-out')
+  setTimeout(function(){
+    cardFront.classList.remove('animation-fade-out')
+    cardFront.classList.add('animation-fade-in')
+  }, 500)
+  
+}
+
 
 function handleCollapsible(event){
     let all_els = Array.from(this.children)
@@ -48,6 +72,12 @@ function init(event){
     let collapsible = document.querySelectorAll('.js-collapsible')
     collapsible.forEach(function(it){
         it.addEventListener('click', handleCollapsible)
+    })
+
+    let translatedCard = document.querySelectorAll('.translated-card__item')
+    translatedCard.forEach(function(it){
+        it.addEventListener('mouseenter', translatedCardAnimationIn)
+        it.addEventListener('mouseleave', translatedCardAnimationOut)
     })
 
     let marquee_el = document.querySelector('.marquee__inner')
